aca4c87340e7b72c349cd249ce5746d18bdb7519
PriscillaT002 <ptrejo@smith.edu>
2022-09-19 22:54:56 -0400

    new changes

--- computer.py
+++ computer.py
@@ -1,10 +1,64 @@
+from typing import Dict, Union, Optional
+
 class Computer:
+    
+    #Method
+    def __init__(self, description : str ,processor_type: int, hard_drive_capacity: int, memory: int, operating_system: str, year_made: int, price: float) -> None:
+    
+    #attributes
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
 
-    # What attributes will it need?
+    #method refurbish
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def refurbish(inventory, item_id: int, new_os: Optional[str] = None):
+     if item_id in inventory:
+        computer = inventory[item_id] # locate the computer
+        if int(computer["year_made"]) < 2000:
+            computer["price"] = 0 # too old to sell, donation only
+        elif int(computer["year_made"]) < 2012:
+            computer["price"] = 250 # heavily-discounted price on machines 10+ years old
+        elif int(computer["year_made"]) < 2018:
+            computer["price"] = 550 # discounted price on machines 4-to-10 year old machines
+        else:
+            computer["price"] = 1000 # recent stuff
 
-    # What methods will you need?
+        if new_os is not None:
+            computer["operating_system"] = new_os # update details after installing new OS
+     else:
+        print("Item", item_id, "not found. Please select another item to refurbish.")
+
+    #method update price
+    def update_price(inventory, item_id: int, new_price: int):
+     if item_id in inventory:
+        inventory[item_id]["price"] = new_price
+     else:
+        print("Item", item_id, "not found. Cannot update price.")
+
+    #method new computer
+    def create_computer(description: str,
+                    processor_type: str,
+                    hard_drive_capacity: int,
+                    memory: int,
+                    operating_system: str,
+                    year_made: int,
+                    price: int):
+     return {'description': description,
+            'processor_type': processor_type,
+            'hard_drive_capacity': hard_drive_capacity,
+            'memory': memory,
+            'operating_system': operating_system,
+            'year_made': year_made,
+            'price': price
+    }
+
+
+
+      
+
+   
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,60 @@
-class ResaleShop:
+from computer import Computer
+from typing import Dict, Union, Optional
 
-    # What attributes will it need?
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
 
-    # What methods will you need?
+class ResaleShop():
+
+    ItemID = 0
+    inventory = {}
+
+    def __init__(self, ItemID: int, inventory):
+        self.inventory: Dict[int, Computer] = inventory
+        self.ItemID = ItemID
+
+        
+
+    def buy(self, computer) -> int:
+        # global itemID
+        # global inventory
+        self.ItemID =+1
+        self.inventory[self.ItemID] = computer
+        return self.ItemID
+
+       
+
+    #method sell
+
+    def sell(inventory, item_id: int ):
+     if item_id in inventory:
+        del inventory[item_id]
+        print("Item", item_id, "sold!")
+     else: 
+        print("Item", item_id, "not")
+
+   
+
+    #method inventory
+
+    def print_inventory(inventory):
+    # If the inventory is not empty
+     if inventory:
+        # For each item
+        for item_id in inventory:
+            # Print its details
+            print(f'Item ID: {item_id} : {inventory[item_id]}')
+     else:
+        print("No inventory to display.")
+
+
+
+
+
+        
+
+
+
+
+
+
+
